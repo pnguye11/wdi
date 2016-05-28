13db0adceaa9773d0d12c0a453c60964e61e480c
Phuong Nathan Nguyen <pnguye11@gmail.com>
2016-05-28 14:46:16 -0700

    initialize repo

---
+++ cf.rb
@@ -0,0 +1,12 @@
+
+  print = "Enter a, b, or c: "
+  begin
+    choice = get.chomp
+    if word == "a"
+    puts "a is for apple"
+    elsif word == "b"
+    puts  " is for banana"
+    else word == "c"
+    puts "is for cantalope"
+  else puts "you're a rebel"
+end
---
+++ learn_ruby.rb
@@ -0,0 +1,43 @@
+# sheep = [1, 2, 3, 4, 5]
+
+# sheep.each do | number |
+#   puts " I am sheep #{number}"
+# end
+# puts '\n'
+
+# sheep.count do |shep|
+#   if shep < 4
+#     puts "I am sheep #{shep}"
+#   else
+#     puts "zzzzz"
+#   end
+# end
+
+# def go_to_sleep goats
+#   goats.each do |goat|
+#     if goat < 4
+#       puts "#{goat} sheep"
+#     else
+#       puts "zzzzz"
+#     end
+#   end
+# end
+
+
+# dreams = ['scary', 'funny', 'woah', 'anxious', 'super-powered']
+
+# dreams.each do |dream|
+#   if dream.include? 's'
+#     dream.prepend "actually "
+#   end
+#  end
+
+
+dream = {
+  duration: 60,
+  clothes: true,
+  genre: "horror",
+  monsters: ["Freddy", "Jason", "Vin Deisel"]
+}
+
+dream[:my_power] =
